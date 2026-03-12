1664f9fbf496a5fff5bf11044df2c49b740d36e4
kishansoni5 <kishansonii2005@gmail.com>
2026-03-12 19:07:06 +0530

    Added LoginServlet with BCrypt and session management

---
+++ src/main/java/com/blog/servlet/LoginServlet.java
@@ -0,0 +1,53 @@
+package com.blog.servlet;
+
+import java.io.IOException;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.blog.dao.UserDAO;
+import com.blog.model.User;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        if (email == null || email.trim().isEmpty() ||
+                password == null || password.trim().isEmpty()) {
+
+            request.setAttribute("error", "All fields are required!");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+            return;
+        }
+        UserDAO userDAO = new UserDAO();
+        User user = userDAO.getUserByEmail(email);
+        if (user == null) {
+            request.setAttribute("error", "Email not registered!");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+            return;
+        } else {
+            if (BCrypt.checkpw(password, user.getPassword())) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user", user);
+                response.sendRedirect("index.jsp");
+            } else {
+                request.setAttribute("error", "Invalid password!");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+            }
+        }
+
+    }
+}
